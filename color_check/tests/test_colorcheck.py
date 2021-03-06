8d976bb230516207bc68367e5953b357e3f23b4e
Mattia Solarino <TiaSola@iMac-di-Mattia.local>
2021-03-06 15:10:58 +0100

    color checker works and error handlers made

--- color_check/tests/test_colorcheck.py
+++ color_check/tests/test_colorcheck.py
@@ -8,8 +8,7 @@ def test_get_color_code():
     # assert get_color_code("blue") == "#0000ff"
     # the following test will fail at the beginning,
     # uncomment when you think you are finished!
-    color_code = get_color_code("red")
-    assert color_code[1] == "#ff0000"
+    assert get_color_code("red") == "#ff0000"
 
 
 # our very first functional test
